65bff287ad719327927a81dcd1770947fb703305
Harsh <harshsharma102000@gmail.com>
2021-03-23 21:28:12 +0530

    Tinder card setup

---
+++ client/src/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
+import tinderLogo from "./images/tinderlogo.png";
+
+function Header() {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontsize="large" className="header__icon" />
+      </IconButton>
+      <img className="header__logo" src={tinderLogo} alt="logo" />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
+    </div>
+  );
+}
+
+export default Header;
